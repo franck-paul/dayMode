3b636a64ff5bec78c2a82c635cf4722f6d044185
franck <carnet.franck.paul@gmail.com>
2023-06-12 10:02:24 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -14,7 +14,7 @@ use Dotclear\Helper\L10n;
 L10n::$locales['Next day'] = 'Journée suivante';
 L10n::$locales['Previous day'] = 'Journée précédente';
 L10n::$locales['Provides daily archives and an associated calendar widget'] = 'Fournit des archives du jour et un widget calendrier associé';
-L10n::$locales['dayMode'] = '';
+L10n::$locales['dayMode'] = 'Mode journalier';
 L10n::$locales['Daily Archives'] = 'Archives journalières';
 L10n::$locales['Enable daily archives and calendar'] = 'Activer les archives journalières et le calendrier';
 L10n::$locales['DayMode: calendar'] = 'DayMode : calendrier';
