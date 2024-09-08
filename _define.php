59fe69c788056436577d4e9ab05507cd565dd27b
franck <carnet.franck.paul@gmail.com>
2024-09-08 09:14:35 +0200

    New release 5.2

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'dayMode',
     'Provides daily archives and an associated calendar widget',
     'Pep and contributors',
-    '5.1.1',
+    '5.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
