d4a2f10c39d4f72a678e258b1c2e27827eb582cd
franck <carnet.franck.paul@gmail.com>
2025-08-02 10:44:08 +0200

    New release 6.2

--- _define.php
+++ _define.php
@@ -14,9 +14,9 @@ $this->registerModule(
     'dayMode',
     'Provides daily archives and an associated calendar widget',
     'Pep and contributors',
-    '6.1.2',
+    '6.2',
     [
-        'date'     => '2025-06-09T22:42:00+0200',
+        'date'     => '2025-08-02T10:44:08+0200',
         'requires' => [
             ['core', '2.34'],
             ['TemplateHelper'],
